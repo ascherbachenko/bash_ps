26b8253d9b5d761285d7a9de73a536a6b7432caf
ascherbachenko <ascherbachenko@mail.ru>
2025-07-13 23:42:57 +0300

    edit list.sh path to folder4

--- hw04/list.sh
+++ hw04/list.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-for file in /var/var/*
-do
-    echo $file
-done
+if [ -d /var/var ]; then
+    for file in /var/var/*
+    do
+        echo $file
+    done
+else
+    echo "Directory not found"
+fi
