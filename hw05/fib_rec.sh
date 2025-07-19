84565dc69d3caa10c76a429b7b4aefb0e50efd67
ascherbachenko <ascherbachenko@mail.ru>
2025-07-19 21:47:15 +0300

    edit fib_recv1.sh

--- hw05/fib_rec.sh
+++ hw05/fib_rec.sh
@@ -2,26 +2,19 @@
 
 # Рекурсивная функция вычисления n-го числа Фибоначчи
 fib() {
-    local n=$1
+    read -p "Введите количество чисел Фибоначчи: " num
+    local n=$num
     if (( n <= 1 )); then
         echo $n
     else
         echo $(( $(fib $((n-1))) + $(fib $((n-2))) ))
     fi
-}
 
-# Функция для вывода ряда Фибоначчи
-print_fibonacci() {
-    local n=$1
-    echo "Ряд Фибоначчи из $n чисел:"
-    for (( i=0; i<n; i++ )); do
+    echo "Ряд Фибоначчи из $num чисел:"
+    for (( i=0; i<num; i++ )); do
         echo -n "$(fib $i) "
     done
-    echo
 }
-
-# Запрос ввода от пользователя
-read -p "Введите количество чисел Фибоначчи: " n
 
 # Проверка ввода
 if ! [[ "$n" =~ ^[0-9]+$ ]] || (( n < 1 )); then
@@ -29,8 +22,8 @@ if ! [[ "$n" =~ ^[0-9]+$ ]] || (( n < 1 )); then
     exit 1
 fi
 
-if [ $n -lt 3 ]; then 
-    echo "Number is wrong"
+if [ $n -le 3 ]; then 
+    echo "Number more 3s"
 else
     print_fibonacci "$n"
 fi
