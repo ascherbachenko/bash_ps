05cd31cb6f6c876402188729287504b29e8d76f4
ascherbachenko <ascherbachenko@mail.ru>
2025-07-28 23:39:03 +0300

    change key in variable v2

--- hw07/change_days.sh
+++ hw07/change_days.sh
@@ -2,9 +2,9 @@
 
 declare -a week=("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" "Sunday")
 
-change_days=${week[4]}
-week[2]=${week[2]}
-week[6]=$change_days
+change_days=${week[2]}
+week[2]=${week[4]}
+week[4]=$change_days
 
 # Show array
 echo ${week[@]}
