cf3dc696da61b0c9fdc20ae5205219f8e01ef17b
ascherbachenko <ascherbachenko@mail.ru>
2025-07-28 23:35:26 +0300

    change index 6 -> 4

--- hw07/change_days.sh
+++ hw07/change_days.sh
@@ -3,7 +3,7 @@
 declare -a week=("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" "Sunday")
 
 change_days=${week[2]}
-week[2]=${week[6]}
+week[2]=${week[4]}
 week[6]=$change_days
 
 # Show array
